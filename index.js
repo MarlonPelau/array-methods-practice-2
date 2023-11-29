3b79691d6b5a0560345f41bc264851b1e2d72b63
MarlonPelau <MarlonRegis@pursuit.org>
2023-11-29 16:54:57 -0500

    finished this array-methods-practice-2

--- index.js
+++ index.js
@@ -47,35 +47,45 @@ const videoGames = [
  * @param {string} name - The name of the game to find.
  * @returns {Object | undefined} The game object if found, otherwise undefined.
  */
-function findGameByName() {}
+function findGameByName(name) {
+  return videoGames.find(game => game.name === name);
+}
 
 /**
  * Retrieves all games available on PlayStation.
  * Use the .filter() array method to filter the games.
  * @returns {Object[]} An array of game objects available on PlayStation.
  */
-function gamesOnPlayStation() {}
+function gamesOnPlayStation() {
+  return videoGames.filter(game => game.consoles.includes("PlayStation"));
+}
 
 /**
  * Gets the names of all games in the dataset.
  * Use the .map() array method to create an array of names.
  * @returns {string[]} An array of names of all games.
  */
-function getAllGameNames() {}
+function getAllGameNames() {
+  return videoGames.map(game => game.name);
+}
 
 /**
  * Calculates the total number of players across all games.
  * Use the .reduce() array method to sum up the players.
  * @returns {number} The total number of players.
  */
-function totalNumberOfPlayers() {}
+function totalNumberOfPlayers() {
+  return videoGames.reduce((totalPlayers, game) => totalPlayers + game.numPlayers, 0);
+}
 
 /**
  * Retrieves all games released after the year 2019.
  * Use the .filter() array method to filter the games.
  * @returns {Object[]} An array of game objects released after 2019.
  */
-function gamesReleasedAfter2019() {}
+function gamesReleasedAfter2019() {
+  return videoGames.filter((game) => game.releaseYear > 2019);
+}
 
 /**
  * Updates the description of a specified game.
@@ -83,35 +93,60 @@ function gamesReleasedAfter2019() {}
  * @param {string} name - The name of the game to update.
  * @param {string} newDescription - The new description for the game.
  */
-function updateGameDescription() {}
+function updateGameDescription(name, newDescription) {
+  const updateGames = videoGames.find((game) => game.name === name);
+  if(updateGames) {
+    updateGames.description = newDescription;
+    return "New Description"
+} else {
+  return "Game Unfound"
+}
+}
 
 /**
  * Creates a list of games with the count of consoles they are available on.
  * Use the .map() array method to create the new array.
  * @returns {Object[]} An array of objects with game name and console count.
  */
-function gamesWithConsoleCount() {}
+function gamesWithConsoleCount() {
+  const result = videoGames.map((game) => {
+    return {
+      name: game.name,
+      consoleCount: game.consoles.length,
+    };
+  });
+  return result;
+  }
 
 /**
  * Finds the game with the maximum number of players.
  * Use the .reduce() method to determine the game with the most players.
  * @returns {Object} The game object with the maximum number of players.
  */
-function gameWithMaxPlayers() {}
+function gameWithMaxPlayers() {
+  return videoGames.reduce((maxPlayers, currentGamers) => currentGamers.numPlayers > maxPlayers.numPlayers ? currentGamers : maxPlayers)
+};
 
 /**
  * Retrieves games available on both PC and Xbox.
  * Use the .filter() array method to filter the games.
  * @returns {Object[]} An array of game objects available on both PC and Xbox.
  */
-function gamesOnPCAndXbox() {}
+function gamesOnPCAndXbox() {
+  return videoGames.filter(game => game.consoles.includes("PC") && game.consoles.includes("Xbox"))
+};
 
 /**
  * Counts the number of games released each year.
  * Use the .reduce() method to count games per year.
  * @returns {Object} An object with years as keys and counts as values.
  */
-function countGamesByYear() {}
+function countGamesByYear() {
+  return videoGames.reduce((countByYr, {releaseYear}) => {
+    countByYr[releaseYear] = (countByYr[releaseYear] || 0) + 1
+    return countByYr;
+  }, {});
+}
 
 module.exports = {
   findGameByName,
